899e27cfb0038f896dc9242cebaac45579d5ab1e
Joaquin <jk83878387@gmail.com>
2026-05-19 12:51:09 -0300

    Cambios en admin

--- app/novedades/page.js
+++ app/novedades/page.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "Novedades | SEG Ingeniería",
   description:
